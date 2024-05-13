057c48cd80f322341d7cfccc8dde3c85bae48f2f
ibrahimyasser450 <ibrahimyasser450@gmail.com>
2024-05-13 03:29:30 +0300

    Change_Password

--- api/login/models.py
+++ api/login/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class LoginPerson(models.Model):   
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
--- api/login/urls.py
+++ api/login/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, ChangePasswordConfirmView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    # path('change_password/', PasswordResetConfirmView.as_view(), name='change_password'),
+    path('change_password/', ChangePasswordConfirmView.as_view(), name='change_password'),
 ]
